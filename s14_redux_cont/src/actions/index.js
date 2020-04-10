14e26e0518074e2dc5ba3ee6bfc34cdc5dc6e1c9
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-04-10 13:39:30 -0400

    s14 alt actions without _.memoize

--- s14_redux_cont/src/actions/index.js
+++ s14_redux_cont/src/actions/index.js
@@ -1,6 +1,21 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 import _ from "lodash";
 
+export const fetchPostsAndUsers = id => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  //   Get array of all unique userIds
+  //   const userIds = _.uniq(_.map(getState().posts, "userId"));
+  //   userIds.forEach(id => dispatch(fetchUser(id)));
+  //no await because we don't care how long this takes, also async/await does not work with forEach
+
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+};
+
 export const fetchPosts = () => async dispatch => {
   const res = await jsonPlaceholder.get("/posts");
 
@@ -15,16 +30,16 @@ export const fetchPosts = () => async dispatch => {
 //     };
 //   };
 
-// export const fetchUser = id => async dispatch => {
-//   const res = await jsonPlaceholder.get(`/users/${id}`);
-
-//   dispatch({ type: "FETCH_USER", payload: res.data });
-// };
-
-// Using lodash (_)
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const res = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: res.data });
-});
+};
+
+// Using lodash (_)
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const res = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: "FETCH_USER", payload: res.data });
+// });
