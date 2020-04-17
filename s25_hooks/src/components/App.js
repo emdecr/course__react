f70ada9f8b7c0555b2c899655533acefe28f70f9
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-04-17 10:46:03 -0400

    s25 pre-hooks ResourceList

--- s25_hooks/src/components/App.js
+++ s25_hooks/src/components/App.js
@@ -1,22 +1,18 @@
 import React, { useState } from "react";
+import ResourceList from "./ResourceList";
 
-class App extends React.Component {
-  state = { resource: "posts" };
-  render() {
-    return (
+const App = () => {
+  // [pieceOfState, setState] = useState(initialValue)
+  const [resource, setResource] = useState("posts");
+  return (
+    <div>
       <div>
-        <div>
-          <button onClick={() => this.setState({ resource: "posts" })}>
-            Posts
-          </button>
-          <button onClick={() => this.setState({ resource: "todos" })}>
-            To-Dos
-          </button>
-        </div>
-        {this.state.resource}
+        <button onClick={() => setResource("posts")}>Posts</button>
+        <button onClick={() => setResource("todos")}>To-Dos</button>
       </div>
-    );
-  }
-}
+      <ResourceList resource={resource} />
+    </div>
+  );
+};
 
 export default App;
---
+++ s25_hooks/src/components/ResourceList.js
@@ -0,0 +1,25 @@
+import React from "react";
+import axios from "axios";
+
+class ResourceList extends React.Component {
+  state = { resources: [] };
+  async componentDidMount() {
+    const res = await axios.get(
+      `https:jsonplaceholder.typicode.com/${this.props.resource}`
+    );
+    this.setState({ resources: res.data });
+  }
+  async componentDidUpdate(prevProps) {
+    if (prevProps.resource !== this.props.resource) {
+      const res = await axios.get(
+        `https:jsonplaceholder.typicode.com/${this.props.resource}`
+      );
+      this.setState({ resources: res.data });
+    }
+  }
+  render() {
+    return <div>Resource: {this.state.resources.length}</div>;
+  }
+}
+
+export default ResourceList;
