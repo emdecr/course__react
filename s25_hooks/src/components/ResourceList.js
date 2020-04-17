84d68e8697e160bc97cfa1bc6692e9477f4454ed
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-04-17 10:46:46 -0400

    s25 add note

--- s25_hooks/src/components/ResourceList.js
+++ s25_hooks/src/components/ResourceList.js
@@ -10,6 +10,7 @@ class ResourceList extends React.Component {
     this.setState({ resources: res.data });
   }
   async componentDidUpdate(prevProps) {
+    //   If you didn't have this check, it would keep making the axios request
     if (prevProps.resource !== this.props.resource) {
       const res = await axios.get(
         `https:jsonplaceholder.typicode.com/${this.props.resource}`
