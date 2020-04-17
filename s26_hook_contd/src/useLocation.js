c2ad0c381d829c4ba4fcffdd81edde58b9380a98
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-04-17 11:58:31 -0400

    s26 remove hooks logic from comp

--- s26_hook_contd/src/useLocation.js
+++ s26_hook_contd/src/useLocation.js
@@ -1,3 +1,17 @@
 import { useState, useEffect } from "react";
 
-export default () => {};
+export default () => {
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    window.navigator.geolocation.getCurrentPosition(
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
+    );
+  }, []);
+  // Passing in an empty array [] as second argument run the function once for the entire lifecycle of component
+
+  //   return { lat: lat, error: errorMessage };
+  return [lat, errorMessage]; //community convention
+};
