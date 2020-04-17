f632c2f3fe41052f23d4a57cf867476a00af6ddd
Emily Dela Cruz <delacruz.emily@gmail.com>
2020-04-17 11:37:06 -0400

    s25 reuse w/ hooks

--- s25_hooks/src/components/App.js
+++ s25_hooks/src/components/App.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import ResourceList from "./ResourceList";
+import UserList from "./UsersList";
 
 const App = () => {
   // [pieceOfState, setState] = useState(initialValue)
   const [resourceType, setResourceType] = useState("posts");
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResourceType("posts")}>Posts</button>
         <button onClick={() => setResourceType("todos")}>To-Dos</button>
--- s25_hooks/src/components/ResourceList.js
+++ s25_hooks/src/components/ResourceList.js
@@ -1,24 +1,5 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-const useResources = resourceType => {
-  const [resources, setResources] = useState([]);
-
-  // If second argument [] changes, the arrow function is called
-  //   Like what was happening with if statement in didComponentUpdate when this was a class comp
-  useEffect(() => {
-    (async resourceType => {
-      const res = await axios.get(
-        `https:jsonplaceholder.typicode.com/${resourceType}`
-      );
-      setResources(res.data);
-    })(resourceType);
-  }, [resourceType]);
-  // NTS: if you pass in an empty array, the function only gets called once
-  //   Like making the call in componentDidMount
-  // NTS: if you don't pass in an array, the function keeps getting called
-  return resources;
-};
+import React from "react";
+import useResources from "./useResources";
 
 const ResourceList = ({ resourceType }) => {
   const resources = useResources(resourceType);
